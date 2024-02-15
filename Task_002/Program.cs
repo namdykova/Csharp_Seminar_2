c092475221c6f37b1e7fcdb1f8db3c81e9ae0e65
Irina <namdykova03@gmail.com>
2024-02-15 17:43:34 +0300

    add Task_002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x < 0 && y > 0) 
+{
+    Console.WriteLine("2");
+}
+else if (x < 0 && y < 0) 
+{
+    Console.WriteLine("3");
+}
+else if (x > 0 && y < 0) 
+{
+    Console.WriteLine("4");
+}
+else
+{
+    Console.WriteLine("Точка находится на оси координат");
+}
