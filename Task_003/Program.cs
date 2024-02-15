14b5364ca8beb9f25b614a64cc35648748adcc9b
Irina <namdykova03@gmail.com>
2024-02-15 17:49:32 +0300

    add Task_003

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число от 10 до 99 включительно:"); 
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9 && number < 100)
+    {
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+} else
+{
+    System.Console.WriteLine("Не число от 10 до 99 включительно");
+}
